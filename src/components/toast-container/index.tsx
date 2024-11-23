4ebb38a7805108a199a7e3c9888eeeec4439f694
nooshin <nooshinbry@gmail.com>
2024-11-23 20:13:14 +0330

    fix: toastify error in server component

---
+++ src/components/toast-container/index.tsx
@@ -0,0 +1,12 @@
+"use client"
+import { ToastContainer } from "react-toastify";
+
+const ToastContainerNOSSR = () => {
+  return (
+    <>
+      <ToastContainer />
+    </>
+  );
+};
+
+export default ToastContainerNOSSR;
